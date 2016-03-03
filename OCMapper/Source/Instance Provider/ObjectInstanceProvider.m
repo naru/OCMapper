95632a215294b93ee9179789039d370c1570260c
Anton Narusberg <anton.narusberg@gmail.com>
2016-03-03 18:16:18 +0200

    ObjectInstanceProvider dict null check

--- OCMapper/Source/Instance Provider/ObjectInstanceProvider.m
+++ OCMapper/Source/Instance Provider/ObjectInstanceProvider.m
@@ -37,86 +37,90 @@
 
 - (id)init
 {
-	if (self = [super init])
-	{
-		self.propertyNameDictionary = [NSMutableDictionary dictionary];
-	}
-	
-	return  self;
+    if (self = [super init])
+    {
+        self.propertyNameDictionary = [NSMutableDictionary dictionary];
+    }
+
+    return  self;
 }
 
 #pragma mark - InstanceProvider Methods -
 
 - (BOOL)canHandleClass:(Class)class
 {
-	static Class managedObjectClass = nil;
-	
-	if (!managedObjectClass)
-		managedObjectClass = NSClassFromString(@"NSManagedObject");
-	
-	return ([class isSubclassOfClass:managedObjectClass]) ? NO : YES;
+    static Class managedObjectClass = nil;
+
+    if (!managedObjectClass)
+        managedObjectClass = NSClassFromString(@"NSManagedObject");
+
+    return ([class isSubclassOfClass:managedObjectClass]) ? NO : YES;
 }
 
 - (id)emptyInstanceForClass:(Class)class
 {
-	return [[class alloc] init];
+    return [[class alloc] init];
 }
 
 - (id)emptyCollectionInstance
 {
-	return [NSMutableArray array];
+    return [NSMutableArray array];
 }
 
 - (id)upsertObject:(NSObject *)object error:(NSError **)error;
 {
-	// Object not stored anywhere and therefore no need to upsert
-	// This is specifically made for NSManagedObjects used by ManagedObjectInstanceProvider
-	
-	return object;
+    // Object not stored anywhere and therefore no need to upsert
+    // This is specifically made for NSManagedObjects used by ManagedObjectInstanceProvider
+
+    return object;
 }
 
 - (NSString *)propertyNameForObject:(NSObject *)object byCaseInsensitivePropertyName:(NSString *)caseInsensitivePropertyName
 {
-	NSString *result = nil;
-	Class currentClass = [object class];
-	
-	NSString *key = [NSString stringWithFormat:@"%@.%@", NSStringFromClass(currentClass), caseInsensitivePropertyName];
-	
-	if (self.propertyNameDictionary[key])
-		return self.propertyNameDictionary[key];
-	
-	// Support underscore case (EX: map first_name to firstName)
-	caseInsensitivePropertyName = [caseInsensitivePropertyName stringByReplacingOccurrencesOfString:@"_" withString:@""];
-	
-	while (currentClass && currentClass != [NSObject class])
-	{
-		unsigned int outCount, i;
-		objc_property_t *properties = class_copyPropertyList(currentClass, &outCount);
-		
-		for (i = 0; i < outCount; i++)
-		{
-			objc_property_t property = properties[i];
-			NSString *propertyName = [NSString stringWithUTF8String:property_getName(property)];
-			
-			if ([[propertyName lowercaseString] isEqual:[caseInsensitivePropertyName lowercaseString]])
-			{
-				result = propertyName;
-				break;
-			}
-		}
-		
-		free(properties);
-		
-		if (result)
-		{
-			self.propertyNameDictionary[key] = result;
-			return result;
-		}
-		
-		currentClass = class_getSuperclass(currentClass);
-	}
-	
-	return nil;
+    if (self.propertyNameDictionary == NULL) {
+        self.propertyNameDictionary = [[NSMutableDictionary alloc] init];
+    }
+
+    NSString *result = nil;
+    Class currentClass = [object class];
+
+    NSString *key = [NSString stringWithFormat:@"%@.%@", NSStringFromClass(currentClass), caseInsensitivePropertyName];
+
+    if (self.propertyNameDictionary[key])
+        return self.propertyNameDictionary[key];
+
+    // Support underscore case (EX: map first_name to firstName)
+    caseInsensitivePropertyName = [caseInsensitivePropertyName stringByReplacingOccurrencesOfString:@"_" withString:@""];
+
+    while (currentClass && currentClass != [NSObject class])
+    {
+        unsigned int outCount, i;
+        objc_property_t *properties = class_copyPropertyList(currentClass, &outCount);
+
+        for (i = 0; i < outCount; i++)
+        {
+            objc_property_t property = properties[i];
+            NSString *propertyName = [NSString stringWithUTF8String:property_getName(property)];
+
+            if ([[propertyName lowercaseString] isEqual:[caseInsensitivePropertyName lowercaseString]])
+            {
+                result = propertyName;
+                break;
+            }
+        }
+        
+        free(properties);
+        
+        if (result)
+        {
+            self.propertyNameDictionary[key] = result;
+            return result;
+        }
+        
+        currentClass = class_getSuperclass(currentClass);
+    }
+    
+    return nil;
 }
 
 @end
